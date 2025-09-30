2ea86b993f73ea17f221500717ceac9f37439b29
Rizky Primadona <primadona.rizky22@gmail.com>
2025-10-01 00:35:01 +0700

    update path peraturan

--- landing_page/www/peraturan/index.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-def get_context(context):
-    context.courses = frappe.get_all(
-        "LMS Course",
-        fields=["name", "title", "image", "short_introduction"],
-        limit=5
-    )
-    return context
